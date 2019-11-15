0ea9306ab57a87bdee40003c63b0997df34becd0
Giovanni Martina <1324915+drayah@users.noreply.github.com>
2019-11-15 17:40:41 -0300

    Implement partial1, curry, uncurry and compose

--- ch2/MyModule.scala
+++ ch2/MyModule.scala
@@ -38,6 +38,18 @@ object MyModule {
     loop(0)
   }
 
+  def partial1[A,B,C](a: A, f: (A, B) => C): B => C =
+    b => f(a, b)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    a => partial1(a, f)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
   def formatResult(name: String, n: Int, f: Int => Int): String =
     "The %s of %d is %d".format(name, n, f(n))
 
